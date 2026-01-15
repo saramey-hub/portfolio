75872e5576a17036b0eb08490a5c90e2102be8cc
saramey-hub <smeehdaoui@gmail.com>
2026-01-15 13:45:02 +0100

    acceuil ajouter et update de app

---
+++ src/pages/Acceuil.js
@@ -0,0 +1,47 @@
+import "../styles/Acceuil.css"
+import image2 from "../assests/image 2.svg"
+import image3 from "../assests/image 3.svg"
+
+function Acceuil() {
+    return (
+
+
+
+        <div className="Section">
+            <div className= "section-card">storyline</div>
+            <div className= "section-card">projects</div>
+            <div className="section-card">resume</div>
+            <div className="section-card">contact</div>
+
+
+
+
+
+
+
+
+
+
+
+        <div className="Acceuil">
+            <h3>my</h3>
+            <h1>PORTFOLIO</h1>
+            <h3>made by sara</h3>
+
+
+            <img src={image2} alt="icon 1"/>
+            <img src={image3} alt="icon 2"/>
+
+
+
+
+        </div>
+
+
+
+        </div>
+    )
+}
+
+
+export default Acceuil;
