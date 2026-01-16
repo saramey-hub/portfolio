4dcb80d22d55700f88395a5c421b0853d52b136b
saramey-hub <smeehdaoui@gmail.com>
2026-01-16 17:58:25 +0100

    icon bouge

--- src/pages/Acceuil.js
+++ src/pages/Acceuil.js
@@ -1,26 +1,30 @@
 import "../styles/Acceuil.css"
 import image2 from "../assests/image 2.svg"
-import image3 from "../assests/image 3.svg"
+
 
 function Acceuil() {
     return (
 
 
 
-        <div className="Section">
-            <div className= "section-card">storyline</div>
-            <div className= "section-card">projects</div>
-            <div className="section-card">resume</div>
-            <div className="section-card">contact</div>
+        <div className="button">
 
+            <div className="buttons">
 
+                <button>about</button>
+                <button>projects</button>
+                <button>resume</button>
+                <button>contact</button>
+            </div>
 
 
 
 
             <div className="Acceuil">
-            <h3>my<img src={image3} alt="icon 2"/></h3>
-            <h1>PORTFOLIO <img src={image2} alt="icon 1"/></h1>
+            <h3>my</h3>
+            <h1>PORTFOLIO
+                <img src={image2} alt="icon 1" className="portfolio-icon" />
+            </h1>
             <h4>made by sara</h4>
 
 
@@ -28,8 +32,8 @@ function Acceuil() {
 
 
 
-        </div>
 
+            </div>
 
 
         </div>
@@ -38,3 +42,5 @@ function Acceuil() {
 
 
 export default Acceuil;
+
+
