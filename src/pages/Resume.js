93d0807f3e3095a7c654c44d8f86445abeb433e9
saramey-hub <smeehdaoui@gmail.com>
2026-01-20 20:00:19 +0100

    continu

---
+++ src/pages/Resume.js
@@ -0,0 +1,11 @@
+import {Link} from "react-router-dom";
+import "../styles/Resume.css"
+import "../assests/image 2.svg"
+
+function Resume() {
+    return (
+       <h1>Resume</h1>
+    );
+}
+
+export default Resume;
