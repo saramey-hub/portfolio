9d63c5230e7e4ab07ffb988be44bb16259cc580d
saramey-hub <smeehdaoui@gmail.com>
2026-01-22 19:38:37 +0100

    projet titre plus fichier

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,22 +1,27 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
+import image2 from "../assests/image 2.svg";
+import "../styles/Projects.css"
 
 function Projects() {
     return (
-        <div>
+        <>
             <div className="button">
-
                 <div className="butons">
-
-                    <Link to="/about">
-                        <button>about</button>
-                    </Link>
+                    <Link to="/about"><button>about</button></Link>
                     <Link to="/projects"><button className="button-about">projects</button></Link>
                     <Link to="/resume"><button>resume</button></Link>
                     <Link to="/contact"><button>contact</button></Link>
-
                 </div>
-        </div>
-        </div>
+            </div>
+
+            <div className="Projets">
+
+                <h1>PROJECTS
+                    <img src={image2} alt="icon 1" className="portfolio-icon" />
+                </h1>
+
+            </div>
+        </>
     );
 }
 
