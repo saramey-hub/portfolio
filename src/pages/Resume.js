c4e7fba6c3bd99be5d246d7f14b2b78c6a6f0434
saramey-hub <smeehdaoui@gmail.com>
2026-01-25 18:44:57 +0100

    page resume

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,13 @@
 import {Link} from "react-router-dom";
 import "../styles/Resume.css"
 import "../assests/image 2.svg"
+import "../assests/image 4.svg"
+import image4 from "../assests/image 4.svg"
+import image2 from "../assests/image 2.svg";
 
 function Resume() {
     return (
-        <div>
+        <>
             <div className="button">
 
                 <div className="butons">
@@ -18,7 +21,29 @@ function Resume() {
 
                 </div>
             </div>
-        </div>
+
+            <div className="text">
+
+<p>you can download my resume by cliking here !
+<img src={image4} alt="icon 2" className="portfoli-icon" />
+
+
+</p>
+
+            </div>
+                <div className="Resume">
+                <h1>RESUME
+                    <img src={image2} alt="icon 1" className="portfolio-icon" />
+                </h1>
+
+
+            </div>
+
+
+
+
+
+        </>
     );
 }
 
